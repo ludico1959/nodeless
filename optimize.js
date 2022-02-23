c1c43661f855ff13505c5a88eee4238925919403
Lucas Dias Coelho <ludico1959@gmail.com>
2022-02-23 14:35:17 -0300

    feat(handler): create the functions to optimize uploaded images

--- optimize.js
+++ optimize.js
@@ -1,18 +1,49 @@
 "use strict";
 
-module.exports.handle = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const AWS = require("aws-sdk");
+const { basename, extname } = require("path");
+const sharp = require("sharp");
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const S3 = new AWS.S3();
+
+module.exports.handle = async ({ Records: records }, context) => {
+  try {
+    await Promise.all(
+      records.map(async (record) => {
+        const { key } = records.s3.object;
+
+        const image = await S3.getObject({
+          Bucket: process.env.bucket,
+          Key: key,
+        }).promise();
+
+        const optimized = await sharp(image.Body)
+          .resize(1280, 720, { fit: inside }, { withoutEnlargement: true })
+          .toFormat("jpeg", { progressive: true }, { quality: 50 })
+          .toBuffer();
+
+        await S3.putObject({
+          Body: optimized,
+          Bucket: process.env.bucket,
+          ContentType: "image/jpeg",
+
+          /* Key: image path.
+           * Example: uploads/nome-da-imagem.png
+           * basename: nome-da-imagem
+           * extname: .png
+           * 'basename(key, extname(key))': returns just the basename, without its extension name.
+           * This way we can add '.jpeg' into the template string below:
+           */
+          Key: `compressed/${basename(key, extname(key))}.jpeg`,
+        }).promise();
+      })
+    );
+
+    return {
+      statusCode: 301,
+      body: {},
+    };
+  } catch (error) {
+    return error;
+  }
 };
